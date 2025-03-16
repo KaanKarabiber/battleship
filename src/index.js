590b12986c4ae4ff63f1fb35579c549f5b8a133a
Kaan Karabiber <[kaankarabiber1@gmail.com]>
2025-03-16 19:18:51 +0300

    add UI functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import './styles.css';
 import { Game } from './game.js';
+import createUI from './createUI.js';
 
 const game = new Game();
+game.player1.setOpponent(game.player2);
+game.player2.setOpponent(game.player1);
+
+const boards = createUI.createDivs();
+createUI.createBoard(boards[0], game.player1);
+createUI.createBoard(boards[1], game.player2);
