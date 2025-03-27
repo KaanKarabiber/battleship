20ee73712fb62aefc772fdb2148fe90a071b355f
Kaan Karabiber <[kaankarabiber1@gmail.com]>
2025-03-27 22:11:23 +0300

    add row and col datasets to cells

--- src/createUI.js
+++ src/createUI.js
@@ -15,11 +15,21 @@ const createUI = {
     return [player1Grid, player2Grid];
   },
   createBoard(playerBoard, player) {
-    player.gameboard.board.forEach((row) => {
-      row.forEach(() => {
+    player.gameboard.board.forEach((row, rowIndex) => {
+      row.forEach((_, colIndex) => {
         const cell = document.createElement('button');
         cell.classList.add('grid-cell');
         playerBoard.append(cell);
+
+        cell.dataset.row = rowIndex;
+        cell.dataset.col = colIndex;
+
+        cell.addEventListener('click', () => {
+          const row = parseInt(cell.dataset.row);
+          const col = parseInt(cell.dataset.col);
+
+          console.log(`Cell clicked at coordinates: (${row}, ${col})`);
+        });
       });
     });
   },
