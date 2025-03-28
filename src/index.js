bff65705d8b979fc288bf1ee89e45eb1824a72ed
Kaan Karabiber <[kaankarabiber1@gmail.com]>
2025-03-28 16:30:43 +0300

    add place ship for testing and game as argument to createBoard

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import createUI from './createUI.js';
 const game = new Game();
 game.player1.setOpponent(game.player2);
 game.player2.setOpponent(game.player1);
-
+game.player2.placeShip();
 const boards = createUI.createDivs();
-createUI.createBoard(boards[0], game.player1);
-createUI.createBoard(boards[1], game.player2);
+createUI.createBoard(boards[0], game.player1, game);
+createUI.createBoard(boards[1], game.player2, game);
