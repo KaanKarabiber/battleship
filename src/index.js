f54cb4108caa14c91fd607008590c6fd7461fdd2
Kaan Karabiber <[kaankarabiber1@gmail.com]>
2025-03-28 20:13:11 +0300

    add some ships for testing

--- src/index.js
+++ src/index.js
@@ -5,7 +5,25 @@ import createUI from './createUI.js';
 const game = new Game();
 game.player1.setOpponent(game.player2);
 game.player2.setOpponent(game.player1);
+game.player1.placeShip([
+  [0, 0],
+  [0, 1],
+  [0, 2],
+]);
+game.player1.placeShip([
+  [0, 3],
+  [0, 4],
+  [0, 5],
+]);
+game.player1.placeShip([
+  [1, 1],
+  [1, 2],
+  [1, 3],
+  [1, 4],
+  [1, 5],
+]);
 game.player2.placeShip();
 const boards = createUI.createDivs();
 createUI.createBoard(boards[0], game.player1, game);
 createUI.createBoard(boards[1], game.player2, game);
+createUI.renderShips(game);
