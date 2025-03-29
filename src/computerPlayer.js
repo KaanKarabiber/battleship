c205979be3f8578da23f7dad9cdaec55699d36fb
Kaan Karabiber <[kaankarabiber1@gmail.com]>
2025-03-29 16:07:43 +0300

    removed random place ship logic from computer to use it both in computer and player

--- src/computerPlayer.js
+++ src/computerPlayer.js
@@ -4,43 +4,6 @@ export class ComputerPlayer extends Player {
   constructor() {
     super('Computer');
   }
-  //places all ships
-  placeShip() {
-    this.shipLengths.forEach((length) => {
-      const coordinates = this.generateRandomCoordinates(length);
-      super.placeShip(coordinates);
-    });
-  }
-
-  generateRandomCoordinates(length) {
-    let coordinates;
-    const isHorizontal = Math.random() < 0.5; // Randomly decide direction
-    let x, y;
-
-    do {
-      coordinates = [];
-      x = Math.floor(Math.random() * 10);
-      y = Math.floor(Math.random() * 10);
-
-      for (let i = 0; i < length; i++) {
-        let newX = isHorizontal ? x + i : x;
-        let newY = isHorizontal ? y : y + i;
-
-        // Ensure it stays within the board
-        if (newX >= 10 || newY >= 10) {
-          coordinates = [];
-          break; // Restart if out of bounds
-        }
-
-        coordinates.push([newX, newY]);
-      }
-    } while (
-      coordinates.length !== length ||
-      !this.gameboard.isValidPlacement(coordinates)
-    );
-
-    return coordinates;
-  }
   attack() {
     let x, y;
     let validAttack = false;
