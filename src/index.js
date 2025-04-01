2b283508d4707af39ed6b09664dbc4199029ddc7
Kaan Karabiber <[kaankarabiber1@gmail.com]>
2025-04-01 18:21:53 +0300

    add drag and drop system

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 import './styles.css';
 import { Game } from './game.js';
-// import createUI from './createUI.js';
+import createUI from './createUI.js';
 
-const modal = document.querySelector('#name-modal');
-const submitButton = document.querySelector('#submit-name');
-const inputField = document.querySelector('#player-name');
-function openModal() {
-  modal.style.display = 'block';
-}
+// const modal = document.querySelector('#name-modal');
+// const submitButton = document.querySelector('#submit-name');
+// const inputField = document.querySelector('#player-name');
+// function openModal() {
+//   modal.style.display = 'block';
+// }
 
-function closeModal() {
-  modal.style.display = 'none';
-}
+// function closeModal() {
+//   modal.style.display = 'none';
+// }
 
-openModal();
-submitButton.addEventListener('click', () => {
-  const playerName = inputField.value.trim();
-  const game = new Game();
-  game.startGame(game, playerName);
-  closeModal();
-});
+// openModal();
+// submitButton.addEventListener('click', () => {
+//   const playerName = inputField.value.trim();
+//   const game = new Game();
+//   game.startGame(game, playerName);
+//   createUI.createDragGrid();
+//   closeModal();
+// });
 
-// const game = new Game();
-// game.startGame(game, 'xd');
+const game = new Game();
+game.startGame(game, 'xd');
+createUI.createDragGrid();
