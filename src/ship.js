43836636617730873ac2bdddbc7dabe102a5155a
Kaan Karabiber <[kaankarabiber1@gmail.com]>
2025-06-08 19:21:09 +0300

    ships now have id orientation and coordinates for UI purposes

--- src/ship.js
+++ src/ship.js
@@ -1,7 +1,10 @@
 export class Ship {
-  constructor(length) {
+  constructor(length, id = null, orientation = null, coordinates = []) {
     this.length = length;
     this.hits = 0;
+    this.id = id;
+    this.orientation = orientation;
+    this.coordinates = coordinates;
   }
   hit() {
     this.hits += 1;
